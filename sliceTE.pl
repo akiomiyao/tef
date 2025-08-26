796bbd2b227d9593ed2524828b5677a6449082c0
Akio Miyao <akiomiyao@gmail.com>
2025-08-26 13:27:31 +0900

    add opptional upper_limit and lower_limit and Usage.

--- sliceTE.pl
+++ sliceTE.pl
@@ -3,6 +3,26 @@
 $target = $ARGV[0];
 $head   = $ARGV[1];
 $tail   = $ARGV[2];
+$llimit = $ARGV[3];
+$ulimit = $ARGV[4];
+
+$llimit = 200 if $llimit == 0;
+$ulimit = 20000 if $ulimit == 0;
+
+if ($ARGV[2] eq ""){
+    print "
+Usage: perl sliceTE.pl ref/fasta_file TE_head_sequence TE_tail_sequence [Lower_limit] [Upper_limit]
+
+Compressed FASTA files (.gz, .bz2, or .xz) are also supported.
+
+Size limits are optional. Default: Lower_limit = 200, Upper_limit = 20000
+
+Example usage:
+    perl sliceTE.pl TAIR10/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz TGATAATAGGTTTTTCAGTC TGCAAAATACACACTTATCA 100 50000
+
+";
+    exit;
+}
 
 $s = {};
 
@@ -51,7 +71,7 @@ foreach $name (sort keys %$s){
 	    $pos += length($tail);
 #	    print ">$name $hpos-$pos $length bp\n$te\n" if $length < 20000;
 #	    print ">$name $hpos-$pos $length bp\n$te\n" if $length > 4000 and $length < 4500;
-	    print ">$name $hpos-$pos $length bp\n$te\n" if $length > 200 and $length < 20000;
+	    print ">$name $hpos-$pos $length bp\n$te\n" if $length > $llimit and $length < $ulimit;
 	}
     }
 }
